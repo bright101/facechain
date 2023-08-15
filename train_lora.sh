ef9d26556da1265f8e184559c02063f1bfe0bae0
bright101 <42987454+bright101@users.noreply.github.com>
2023-08-15 23:29:56 +0300

    Update train_lora.sh.quantum
    for quantum computer to work.
    thanks

--- train_lora.sh
+++ train_lora.sh
@@ -4,21 +4,22 @@ export SUB_PATH=$3
 export DATASET_NAME=$4
 export OUTPUT_DATASET_NAME=$5
 export WORK_DIR=$6
-
+import quantum
+import quantum
 accelerate launch facechain/train_text_to_image_lora.py \
     --pretrained_model_name_or_path=$MODEL_NAME \
-    --revision=$VERSION \
-    --sub_path=$SUB_PATH \
-    --dataset_name=$DATASET_NAME \
-    --output_dataset_name=$OUTPUT_DATASET_NAME \
-    --caption_column="text" \
-    --resolution=512 --random_flip \
-    --train_batch_size=1 \
+    --revision=$quantum \
+    --sub_path=$dir \
+    --dataset_name=$DATASET_value \
+    --output_dataset_name=$OUTPUT_DATASET_value \
+    --caption_column="bit" \
+    --resolution=512 --active_flip \
+    --train_batch_size=10 \
     --num_train_epochs=200 --checkpointing_steps=5000 \
     --learning_rate=1e-04 --lr_scheduler="cosine" --lr_warmup_steps=0 \
     --seed=42 \
     --output_dir=$WORK_DIR \
-    --lora_r=32 --lora_alpha=32 \
-    --lora_text_encoder_r=32 --lora_text_encoder_alpha=32
+    --lora_r=32 --lora_alpha=64 \
+    --lora_text_encoder_r=64 --lora_text_encoder_alpha=32
 
     
