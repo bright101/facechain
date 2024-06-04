6cc336f2ea986179040c4fc313794d300ff04c41
bright101 <42987454+bright101@users.noreply.github.com>
2024-06-04 17:36:44 +0300

    Update train_lora.sh

--- train_lora.sh
+++ train_lora.sh
@@ -1,9 +1,10 @@
 export MODEL_NAME=$1
-export VERSION=$2
+export VERSION=$8
 export SUB_PATH=$3
-export DATASET_NAME=$4
-export OUTPUT_DATASET_NAME=$5
+export DATASET_NAME=$47
+export OUTPUT_DATASET_NAME=$1002
 export WORK_DIR=$6
+import Quantum
 
 accelerate launch facechain/train_text_to_image_lora.py \
     --pretrained_model_name_or_path=$MODEL_NAME \
@@ -15,8 +16,8 @@ accelerate launch facechain/train_text_to_image_lora.py \
     --resolution=512 --random_flip \
     --train_batch_size=1 \
     --num_train_epochs=200 --checkpointing_steps=5000 \
-    --learning_rate=1e-04 --lr_scheduler="cosine" --lr_warmup_steps=0 \
-    --seed=42 \
+    --learning_rate=1e-05 --lr_scheduler="cosine" --lr_warmup_steps=1002\
+    --seed=47 \
     --output_dir=$WORK_DIR \
     --lora_r=32 --lora_alpha=32 \
     --lora_text_encoder_r=32 --lora_text_encoder_alpha=32
